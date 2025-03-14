45747180817329cfc7dbbd7cd4a9f0d3d644ac7c
Luluscript <ludivine.tulcibiez@gmail.com>
2025-03-14 16:16:25 +0100

    new asset + env DB

--- dagster_university/dagster_university/assets/metrics.py
+++ dagster_university/dagster_university/assets/metrics.py
@@ -1,3 +1,97 @@
-import dagster as dg
-
+from dagster import asset
+import matplotlib.pyplot as plt
+import geopandas as gpd
+import duckdb
+import os
 from . import constants
+from dagster._utils.backoff import backoff
+import pandas as pd
+
+
+
+@asset(
+    deps=["taxi_trips", "taxi_zones"]
+)
+def manhattan_stats() -> None:
+    """
+    Calcule les statistiques des trajets en taxi pour Manhattan et les stocke au format GeoJSON.
+    """
+    query = """
+        SELECT
+            zones.zone,
+            zones.borough,
+            zones.geometry,
+            COUNT(1) AS num_trips
+        FROM trips
+        LEFT JOIN zones ON trips.pickup_zone_id = zones.zone_id
+        WHERE borough = 'Manhattan' AND geometry IS NOT NULL
+        GROUP BY zone, borough, geometry
+    """
+
+    conn = duckdb.connect(os.getenv("DUCKDB_DATABASE"))
+    trips_by_zone = conn.execute(query).fetch_df()
+
+    trips_by_zone["geometry"] = gpd.GeoSeries.from_wkt(trips_by_zone["geometry"])
+    trips_by_zone = gpd.GeoDataFrame(trips_by_zone)
+
+    with open(constants.MANHATTAN_STATS_FILE_PATH, 'w') as output_file:
+        output_file.write(trips_by_zone.to_json())
+
+
+@asset(
+    deps=["manhattan_stats"]
+)
+def manhattan_map() -> None:
+    """
+    Génère une carte des trajets en taxi à Manhattan et l'enregistre sous forme d'image.
+    """
+    trips_by_zone = gpd.read_file(constants.MANHATTAN_STATS_FILE_PATH)
+
+    fig, ax = plt.subplots(figsize=(10, 10))
+    trips_by_zone.plot(column="num_trips", cmap="plasma", legend=True, ax=ax, edgecolor="black")
+    ax.set_title("Nombre de trajets par zone de taxi à Manhattan")
+
+    ax.set_xlim([-74.05, -73.90])
+    ax.set_ylim([40.70, 40.82])
+    
+    plt.savefig(constants.MANHATTAN_MAP_FILE_PATH, format="png", bbox_inches="tight")
+    plt.close(fig)
+
+
+
+
+# Création de l'asset trips_by_week
+@asset(deps=["taxi_trips"])
+def trips_by_week():
+    """Agrège les trajets par semaine et stocke les résultats dans un fichier CSV."""
+
+    # Requête pour agréger les trajets par semaine
+    query = """
+        SELECT 
+            DATE_TRUNC('week', pickup_datetime) AS period,
+            COUNT(*) AS num_trips,
+            SUM(passenger_count) AS passenger_count,
+            SUM(total_amount) AS total_amount,
+            SUM(trip_distance) AS trip_distance
+        FROM trips
+        WHERE pickup_datetime BETWEEN '2023-03-01' AND '2023-03-31'
+        GROUP BY 1
+        ORDER BY 1;
+    """
+    conn = backoff(
+        fn=duckdb.connect,
+        retry_on=(RuntimeError, duckdb.IOException),
+        kwargs={
+            "database": os.getenv("DUCKDB_DATABASE"),
+        },
+        max_retries=10,
+    )
+
+    # Exécuter la requête et récupérer le DataFrame
+    trips_weekly = conn.execute(query).fetch_df()
+
+    # Conversion de la colonne 'period' en format string (dimanche de la semaine)
+    trips_weekly["period"] = pd.to_datetime(trips_weekly["period"]).dt.strftime("%Y-%m-%d")
+
+    with open(constants.TRIPS_BY_WEEK_FILE_PATH, "w", encoding="utf-8") as output_file:
+        output_file.write(trips_weekly.to_csv())
--- dagster_university/dagster_university/assets/trips.py
+++ dagster_university/dagster_university/assets/trips.py
@@ -1,2 +1,115 @@
 import requests
 from . import constants
+from pathlib import Path
+import duckdb
+import os
+from dagster import asset
+from dagster._utils.backoff import backoff
+
+
+def taxi_trips_file() -> None:
+    """
+    Récupère les fichiers Parquet bruts des trajets en taxi.
+    Sourced from the NYC Open Data portal.
+    """
+    month_to_fetch = '2023-03'
+    raw_trips = requests.get(
+        f"https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{month_to_fetch}.parquet"
+    )
+
+    with open(constants.TAXI_TRIPS_TEMPLATE_FILE_PATH.format(month_to_fetch), "wb") as output_file:
+        output_file.write(raw_trips.content)
+
+
+
+
+@asset
+def taxi_trips_file() -> None:
+    """
+    Récupère les fichiers Parquet bruts des trajets en taxi.
+    """
+    month_to_fetch = '2023-03'
+    raw_trips = requests.get(
+        f"https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{month_to_fetch}.parquet"
+    )
+
+    with open(constants.TAXI_TRIPS_TEMPLATE_FILE_PATH.format(month_to_fetch), "wb") as output_file:
+        output_file.write(raw_trips.content)
+
+
+# 📌 Création de l'asset taxi_zones_file
+@asset
+def taxi_zones_file() -> None:
+    """Télécharge les données des zones de taxi et les enregistre en CSV."""
+    zones_files = requests.get("https://community-engineering-artifacts.s3.us-west-2.amazonaws.com/dagster-university/data/taxi_zones.csv")
+    
+    with open(constants.TAXI_ZONES_FILE_PATH, "wb") as output_file:
+        output_file.write(zones_files.content)
+
+
+
+
+
+@asset(
+deps=["taxi_trips_file"]
+)
+def taxi_trips() -> None:
+    """
+    Le jeu de données brut des trajets en taxi, chargé dans une base de données DuckDB.
+    """
+    query = """
+        CREATE OR REPLACE TABLE trips AS (
+          SELECT
+            VendorID AS vendor_id,
+            PULocationID AS pickup_zone_id,
+            DOLocationID AS dropoff_zone_id,
+            RatecodeID AS rate_code_id,
+            payment_type AS payment_type,
+            tpep_dropoff_datetime AS dropoff_datetime,
+            tpep_pickup_datetime AS pickup_datetime,
+            trip_distance AS trip_distance,
+            passenger_count AS passenger_count,
+            total_amount AS total_amount
+          FROM 'data/raw/taxi_trips_2023-03.parquet'
+        );
+    """
+
+    conn = backoff(
+        fn=duckdb.connect,
+        retry_on=(RuntimeError, duckdb.IOException),
+        kwargs={
+            "database": os.getenv("DUCKDB_DATABASE"),
+        },
+        max_retries=10,
+    )
+    conn.execute(query)
+
+
+
+@asset(
+deps=["taxi_zones_file"]
+)
+def taxi_zones() -> None:
+    """
+    Le jeu de données brut des zones en taxi, chargé dans une base de données DuckDB.
+    """
+    query = """
+        CREATE OR REPLACE TABLE zones AS (
+          SELECT
+            LocationID AS zone_id,
+            zone AS zone,
+            borough AS borough,
+            the_geom AS geometry,  
+          FROM 'data/raw/taxi_zones.csv'
+        );
+    """
+
+    conn = backoff(
+        fn=duckdb.connect,
+        retry_on=(RuntimeError, duckdb.IOException),
+        kwargs={
+            "database": os.getenv("DUCKDB_DATABASE"),
+        },
+        max_retries=10,
+    )
+    conn.execute(query)
